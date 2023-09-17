3b1efccb0dff825de7a2c18fe66257b0f8420b06
chanwoo <mdsoo55828@gmail.com>
2023-09-17 17:24:38 +0900

    Initial commit

---
+++ src/main/java/auto/aws/controller/TestController.java
@@ -0,0 +1,21 @@
+package auto.aws.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String test(){
+        return "aws auto test";
+    }
+
+
+//    @GetMapping("/test/v1")
+//    public String test1(){
+//        return "aws auto update";
+//    }
+
+}
