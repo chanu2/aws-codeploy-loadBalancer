de470a7948c60e34eca6666175dc6310d6887bff
chanwoo <mdsoo55828@gmail.com>
2023-09-18 20:05:40 +0900

    test

--- src/main/java/auto/aws/controller/TestController.java
+++ src/main/java/auto/aws/controller/TestController.java
@@ -2,6 +2,7 @@ package auto.aws.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,5 +18,10 @@ public class TestController {
 //    public String test1(){
 //        return "aws auto update";
 //    }
+
+    @GetMapping("/health")
+    public String check(){
+        return "로드벨런서 성공";
+    }
 
 }
