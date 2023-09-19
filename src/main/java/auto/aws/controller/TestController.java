dc6317eee145ace9273a55a016ff2f53b86a226e
chanwoo <mdsoo55828@gmail.com>
2023-09-19 16:28:56 +0900

    test

--- src/main/java/auto/aws/controller/TestController.java
+++ src/main/java/auto/aws/controller/TestController.java
@@ -1,10 +1,14 @@
 package auto.aws.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 @RestController
 public class TestController {
@@ -19,16 +23,17 @@ public class TestController {
         return "aws auto update";
     }
 
-    @GetMapping("/health")
-    @ResponseStatus(code = HttpStatus.OK)
-    public String check(){
-        return "로드벨런서 성공";
-    }
+//    @GetMapping("/health")
+//    @ResponseStatus(code = HttpStatus.OK)
+//    public String check(){
+//        return "로드벨런서 성공";
+//    }
 
-    @GetMapping("/")
-    @ResponseStatus(code = HttpStatus.OK)
-    public String check1(){
-        return "로드벨런서 성공";
+
+    @GetMapping("/health")
+    public String home(HttpServletRequest request) throws UnknownHostException {
+        InetAddress localHost = InetAddress.getLocalHost();
+        return "Server IP : " + localHost.getHostAddress();
     }
 
 
